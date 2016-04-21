3594b196a7bdad51faede12aa2740251020e9efc
DEVON DAVIAU <devon.daviau@yahoo.ca>
2016-04-21 10:45:07 -0400

    Add dice roller functionality

--- app.js
+++ app.js
@@ -5,8 +5,12 @@
  */
 (function(){
 
-    // Put DOM references into variables
+// Put DOM references into variables
+
+    "use strict";
+    console.log('Hello.');
     var die1Pic = document.getElementById('die1-pic');
+
     var die1Text = document.getElementById('die1');
 
     var die2Pic = document.getElementById('die2-pic');
@@ -14,8 +18,17 @@
 
     var button = document.getElementById('roll-em');
 
-    var counter;
+    var random;
 
+    button.addEventListener('click', function () {
+        random = Math.floor((Math.random() * 6) + 1);
+        die1Pic.setAttribute('src', 'img/' + random + '.png');
+        die1Text.innerText = random;
+        console.log(random);
+        random = Math.floor((Math.random() * 6) + 1);
+        die2Pic.setAttribute('src', 'img/' + random + '.png');
+        die2Text.innerText = random;
+        console.log(random);
+    });
 
-
-});
+})();
