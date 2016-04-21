af4d55ee6d08bd49ee6bc091ee463ae9b72832f5
DEVON DAVIAU <devon.daviau@yahoo.ca>
2016-04-21 09:47:13 -0400

    add dom references to app.js

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
 /**
  * Created by devon on 2016-04-21.
+ * COMP1073-Final Exam
+ * Simple Dice Roller Interface
  */
+(function(){
+
+    // Put DOM references into variables
+    var die1Pic = document.getElementById('die1-pic');
+    var die1Text = document.getElementById('die1');
+
+    var die2Pic = document.getElementById('die2-pic');
+    var die2Text = document.getElementById('die2');
+
+    var button = document.getElementById('roll-em');
+
+    var counter;
+
+    
+
+});
